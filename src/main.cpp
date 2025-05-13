59ef00e9f4c4e41bb620813b9240cc83b3339a28
Dawood Sulaiman Syed <151532509+11xdeveloper@users.noreply.github.com>
2025-05-14 01:44:17 +0500

    feat: load words from files

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+
 #include "raylib.h"
+#include "WordLoader.h"
 
 int main ()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	InitWindow(1280, 800, "Wordle");
+
+	std::vector<std::string> possibleAnswers = loadWordFile("resources/possible_answers.txt");
+	std::vector<std::string> allowedGuesses = loadWordFile("resources/allowed_guesses.txt");
+
+
+	std::cout << "Possible Answers: " << possibleAnswers.size() << std::endl;
+	std::cout << "Allowed Guesses: " << allowedGuesses.size() << std::endl;
 
 	while (!WindowShouldClose())
 	{
