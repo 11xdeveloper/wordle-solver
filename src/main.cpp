79fef86aed08e469876ea9861c401b8322531591
Dawood Sulaiman Syed <151532509+11xdeveloper@users.noreply.github.com>
2025-05-17 19:33:36 +0500

    feat: remove game states

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,14 @@
 #include "WordleSolver.h"
 #include "Utils.h"
 
-enum class GameState {
-	PLAYING,
-	LOST,
-	WON
-};
-
 int main()
 {
 	const int TILE_SIZE = 64;
 	const int SPACING = 10;
 
 	// 5 letters, 6 guesses with spacing
-	const int WINDOW_WIDTH = TILE_SIZE * 5 + SPACING * 6;
+	const int TILES_WIDTH = TILE_SIZE * 5 + SPACING * 6;
+	const int WINDOW_WIDTH = TILES_WIDTH + 300;
 	const int WINDOW_HEIGHT = TILE_SIZE * 6 + SPACING * 7;
 
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -53,11 +48,10 @@ int main()
 
 	WordleSolver solver(words);
 
-	GameState gameState = GameState::PLAYING;
-	Vector2 mousePosition = { 0, 0 };
 
 	std::string input;
 	Result characterResults[5] = { Result::PENDING, Result::PENDING, Result::PENDING, Result::PENDING, Result::PENDING };
+	Vector2 mousePosition = { 0, 0 };
 
 	Guess guesses[6] = {};
 	int currentRow = 0;
@@ -66,145 +60,112 @@ int main()
 
 	while (!WindowShouldClose())
 	{
-		switch (gameState)
+		BeginDrawing();
+
+		ClearBackground(bgColor);
+
+		mousePosition = GetMousePosition();
+
+		int key = GetCharPressed();
+		int inputLength = input.length();
+
+		// a-z, A-Z
+		if (((key >= 65 && key <= 90) || (key >= 97 && key <= 122)) && (inputLength < 5))
 		{
-		case GameState::PLAYING: {
-			BeginDrawing();
+			input.push_back(std::tolower(key));
+		}
 
-			ClearBackground(bgColor);
+		if (IsKeyPressed(KEY_BACKSPACE) && inputLength > 0)
+		{
+			input.pop_back();
+		}
 
-			mousePosition = GetMousePosition();
+		if (IsKeyPressed(KEY_ENTER) && inputLength == 5 && currentRow < 6 && std::find(words.begin(), words.end(), input) != words.end())
+		{
+			Guess guess;
+			guess.word = input;
 
-			int key = GetCharPressed();
-			int inputLength = input.length();
+			bool valid = true;
 
-			// a-z, A-Z
-			if (((key >= 65 && key <= 90) || (key >= 97 && key <= 122)) && (inputLength < 5))
-			{
-				input.push_back(std::tolower(key));
-			}
-
-			if (IsKeyPressed(KEY_BACKSPACE) && inputLength > 0)
-			{
-				input.pop_back();
-			}
-
-			if (IsKeyPressed(KEY_ENTER) && inputLength == 5 && currentRow < 6 && std::find(words.begin(), words.end(), input) != words.end())
-			{
-				Guess guess;
-				guess.word = input;
-
-				bool valid = true;
-
-				for (int i = 0; i < 5; ++i) {
-					std::cout << characterResults[i] << std::endl;
-					if (characterResults[i] == Result::PENDING) {
-						valid = false;
-						break;
-					}
-
-					guess.results[i] = characterResults[i];
+			for (int i = 0; i < 5; ++i) {
+				std::cout << characterResults[i] << std::endl;
+				if (characterResults[i] == Result::PENDING) {
+					valid = false;
+					break;
 				}
 
-				if (valid) {
-					solver.addGuess(guess);
-					guesses[currentRow] = guess;
-					input = "";
-					std::fill(std::begin(characterResults), std::end(characterResults), Result::PENDING);
-					currentRow++;
-
-					int guessesNo = nextGuesses.size();
-					for (int i = 0; i < 10; i++) {
-						if (guessesNo <= i) break;
-						std::cout << "#" << i + 1 << ": " << nextGuesses[i].first << " - " << nextGuesses[i].second << std::endl;
-					}
-				}
+				guess.results[i] = characterResults[i];
 			}
 
-			for (int i = 0; i < 6; i++) {
-				std::string word = i == currentRow ? input : guesses[i].word;
-				for (int j = 0; j < 5; j++) {
-					Rectangle rect = { j * TILE_SIZE + SPACING * (j + 1), i * TILE_SIZE + SPACING * (i + 1), TILE_SIZE, TILE_SIZE };
+			if (valid) {
+				solver.addGuess(guess);
+				guesses[currentRow] = guess;
+				input = "";
+				std::fill(std::begin(characterResults), std::end(characterResults), Result::PENDING);
+				currentRow++;
 
-					// Input tile clicked
-					if (i == currentRow && IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && mousePosition.x > rect.x && mousePosition.x < rect.x + rect.width) {
-						Result r;
-						bool isValid = true;
+				int guessesNo = nextGuesses.size();
+				for (int i = 0; i < 10; i++) {
+					if (guessesNo <= i) break;
+					std::cout << "#" << i + 1 << ": " << nextGuesses[i].first << " - " << nextGuesses[i].second << std::endl;
+				}
+			}
+		}
 
-						if (i == currentRow) {
-							if (characterResults[j] == Result::PENDING) {
-								isValid = false;
-							}
-							else {
-								r = characterResults[j];
-							}
+		for (int i = 0; i < 6; i++) {
+			std::string word = i == currentRow ? input : guesses[i].word;
+			for (int j = 0; j < 5; j++) {
+				Rectangle rect = { j * TILE_SIZE + SPACING * (j + 1), i * TILE_SIZE + SPACING * (i + 1), TILE_SIZE, TILE_SIZE };
+
+				// Input tile clicked
+				if (i == currentRow && IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && mousePosition.x > rect.x && mousePosition.x < rect.x + rect.width) {
+					Result r;
+					bool isValid = true;
+
+					if (i == currentRow) {
+						if (characterResults[j] == Result::PENDING) {
+							isValid = false;
 						}
 						else {
-							r = guesses[i].results[j];
-						}
-
-						if (isValid) {
-							if (r == Result::CORRECT) {
-								DrawRectangleRec(rect, correctColor);
-							}
-							else if (r == Result::PRESENT) {
-								DrawRectangleRec(rect, presentColor);
-							}
-							else if (r == Result::ABSENT) {
-								DrawRectangleRec(rect, absentColor);
-							}
+							r = characterResults[j];
 						}
 					}
+					else {
+						r = guesses[i].results[j];
+					}
 
-
-					DrawRectangleLinesEx(rect, 2, absentColor);
-
-					if (j < word.length()) {
-						std::string temp = word.substr(j, 1);
-						const char* letter = temp.c_str();
-						char lower = tolower(letter[0]);
-
-						Vector2 offset = MeasureTextEx(font, letter, TILE_SIZE * 0.75, 0);
-
-						DrawTextEx(font, letter, { rect.x + TILE_SIZE / 2 - offset.x / 2 , rect.y + TILE_SIZE / 2 - offset.y / 2 }, TILE_SIZE * 0.75, 0, textColor);
-
+					if (isValid) {
+						if (r == Result::CORRECT) {
+							DrawRectangleRec(rect, correctColor);
+						}
+						else if (r == Result::PRESENT) {
+							DrawRectangleRec(rect, presentColor);
+						}
+						else if (r == Result::ABSENT) {
+							DrawRectangleRec(rect, absentColor);
+						}
 					}
 				}
+
+
+				DrawRectangleLinesEx(rect, 2, absentColor);
+
+				if (j < word.length()) {
+					std::string temp = word.substr(j, 1);
+					const char* letter = temp.c_str();
+					char lower = tolower(letter[0]);
+
+					Vector2 offset = MeasureTextEx(font, letter, TILE_SIZE * 0.75, 0);
+
+					DrawTextEx(font, letter, { rect.x + TILE_SIZE / 2 - offset.x / 2 , rect.y + TILE_SIZE / 2 - offset.y / 2 }, TILE_SIZE * 0.75, 0, textColor);
+
+				}
 			}
-
-			EndDrawing();
-			break;
 		}
 
-		case GameState::LOST: {
-			BeginDrawing();
-			ClearBackground(bgColor);
-
-			const char* text = "You lost!";
-
-			Vector2 offset = MeasureTextEx(font, text, 60, NULL);
-
-			DrawTextEx(font, text, { WINDOW_WIDTH / 2 - offset.x / 2, WINDOW_HEIGHT / 2 - offset.y / 2 }, 60, NULL, RED);
-
-			EndDrawing();
-			break;
-		}
-
-		case GameState::WON: {
-			BeginDrawing();
-			ClearBackground(bgColor);
-
-			const char* text = "You won!";
-
-			Vector2 offset = MeasureTextEx(font, text, 60, NULL);
-
-			DrawTextEx(font, text, { WINDOW_WIDTH / 2 - offset.x / 2, WINDOW_HEIGHT / 2 - offset.y / 2 }, 60, NULL, GREEN);
-
-			EndDrawing();
-			break;
-		}
-		}
+		EndDrawing();
 	}
+
 
 	CloseWindow();
 	return 0;
